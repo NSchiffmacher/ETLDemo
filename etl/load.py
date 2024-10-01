aecd2c3e6dc18d1b26c3a97f2611575da82940f1
t.bodin <tanguy.bodin@student.isae-supaero.fr>
2024-10-01 15:27:52 +0200

    Changes

--- etl/load.py
+++ etl/load.py
@@ -4,35 +4,67 @@ import pandas as pd
 from datetime import datetime
 import pymongo
 
+station_json_path = '../data/toulouse.json'
 
 class MongoDBStore:
-    def __init__(self, path: str):
-        pass
+    def __init__(self):
+        self.mongo_client = pymongo.MongoClient()
+        self.database = self.mongo_client['velouze']
+        self.real_time_data_collection = self.database['real_time_data']
+        self.station_information = self.database['station_information']
+        
     
     def commit(self) -> None:
-        # Saves the data to the DB
+        # Saves the data to the DB, not needed here
         pass        
 
     def close(self) -> None:
-        pass
+        self.mongo_client.close()
 
     def save_real_time_stations_summaries(self, all_stations_summaries: list[dict[str, Any]]) -> int:
         """
         Save the real time data of the stations in the store, returning the number of new data added
         """
-        pass
+        new_real_time_data_count = 0
+
+        for station_summary in all_stations_summaries:
+            station_id = station_summary['number']
+            station_last_update = station_summary['last_update']
+
+            last_station_state = self.get_one_station_last_state(station_id)
+            if last_station_state is None or last_station_state['last_update'] < station_last_update:
+                self.real_time_data_collection.insert_one(station_summary)
+                new_real_time_data_count += 1
+
+        return new_real_time_data_count
 
     def get_station_real_time_data(self, station_id: int) -> pd.DataFrame:
         """
         Get the data of a specific station
         """
-        pass
+        pd.DataFrame(self.real_time_data_collection.find({'number': station_id})).drop(columns='_id')
+
+    def get_one_station_last_state(self, station_id : int) -> dict[str, Any]:
+        """
+        Get the last state of a specific station
+        """
+        elements = list(self.real_time_data_collection.find({'number': station_id}).sort('last_update', pymongo.DESCENDING).limit(1))
+        if len(elements) == 0:
+            return None
+        return elements[0]
+
+    def get_all_stations_id(self) -> list[int]:
+        """
+        Get all the stations IDs
+        """
+        return self.station_information.distinct('number')
+    
 
     def get_all_stations_real_time_data(self) -> pd.DataFrame:
         """
         Get the data of all the stations
         """
-        pass
+        pd.DataFrame(self.real_time_data_collection.find()).drop(columns='_id')
 
 
 class JSONStore:
@@ -100,5 +132,6 @@ class JSONStore:
         df.set_index('last_update', inplace=True)
         return df
 
-def DefaultStore() -> JSONStore:
-    return JSONStore('data/real_time.json')
+def DefaultStore() -> MongoDBStore:
+    return MongoDBStore()
+    #return JSONStore('data/real_time.json')
--- setup_mongo.py
+++ setup_mongo.py
@@ -1,5 +1,16 @@
 import pymongo
+import json
+
+station_json_path = '../data/toulouse.json'
 
 if __name__ == "__main__":
     mongo_client = pymongo.MongoClient()
-    db = mongo_client['velouze']
+    database = mongo_client['velouze']
+    real_time_data_collection = database['real_time_data']
+    station_information = database['station_information']
+    station_information.drop()
+    
+    station_information = database['station_information']
+    with open(station_json_path, 'r') as file:
+            data = json.load(file)
+    station_information.insert_many(data)
