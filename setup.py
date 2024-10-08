af30c7b9647623b368d3d8a2de54ce6883477e43
ElBrazilian <nathan.schiffmacher@gmail.com>
2024-10-08 11:20:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,24 +3,28 @@ import pymongo
 from pymongo.server_api import ServerApi
 import os
 
-MONGODB_PSW = os.environ['MONGODB_PSW']
+from etl.load import MongoDBStore
+
 STATION_JSON_PATH = 'data/toulouse.json'
 
 if __name__ == '__main__':
     # Setup log file
+    print('Creating/Resetting log file')
     with open('logs/real_time_extraction.log', 'w') as file:
         file.write('') # Rewrite it to empty
 
     # Setup station information MongoDB
-    uri = f"mongodb+srv://nathan:{MONGODB_PSW}@veloutoulouse.q2ceg.mongodb.net/?retryWrites=true&w=majority&appName=VelouToulouse"
-    mongo_client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
-    database = mongo_client['velouze']
+    print('Setting up station information collection')
+    store = MongoDBStore()
+    database = store.mongo_client['velouze']
     real_time_data_collection = database['real_time_data']
     station_information = database['station_information']
     station_information.drop()
-    #real_time_data_collection.drop()
+    #real_time_data_collection.drop() # Reset the real time DB, not done here 
     
     station_information = database['station_information']
     with open(STATION_JSON_PATH, 'r') as file:
             data = json.load(file)
     station_information.insert_many(data)
+
+    print('Done.')
